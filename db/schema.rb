b23883b8fec2a2aeb63d1332c2a452544cb0a7ca
mlsloynaz <>
2023-02-10 13:41:38 -0500

    added one-many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_061306) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_182541) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
@@ -24,4 +24,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_061306) do
     t.string "image_file_name", default: "placeholder.png"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.integer "stars"
+    t.text "comment"
+    t.integer "movie_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["movie_id"], name: "index_reviews_on_movie_id"
+  end
+
+  create_table "your_scaffolds", force: :cascade do |t|
+    t.string "mystring"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "reviews", "movies"
 end
